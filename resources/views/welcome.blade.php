caa10c3a0855760a0f7f334aef8a93af35c6d95f
Apecengo <apecengo@gmail.com>
2016-11-27 17:09:24 +0100

    Changed welcome page
    Removed placeholder
    Changed subtitle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,18 @@
       <br><br>
       <h1 class="header center black-text">Apply for our server</h1>
       <div class="row center">
-        <h5 class="header col s12 light">You are about to join us.</h5>
+        <h5 class="header col s12 light">Just type your username below.</h5>
       </div>
       <div class="row center">
         
         <form onSubmit="return userName();">
         <div class="row">
         <div class="col s12 m6 offset-m3">
-            <input id="username" name="username" type="text" class="validate" placeholder="Your minecraft username" required>
+            <input id="username" name="username" type="text" class="validate" required>
         </div>
         </div>
 
-        <button type="submit" id="download-button" class="btn-large waves-effect waves-light green accent-4">Get Started</button>
+        <button type="submit" id="download-button" class="btn waves-effect waves-light green accent-4">Get Started</button>
         </form>
       </div>
 
