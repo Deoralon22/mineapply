6a37a29e3802d96c0ba63c977fa95da2cf316b9d
Apecengo <apecengo@gmail.com>
2016-11-27 15:19:01 +0100

    Add Application model

---
+++ database/migrations/2016_11_27_141531_create_applications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uuid');
+            $table->string('username');
+            $table->string('ip');
+            $table->string('email');
+            $table->text('message');
+            $table->integer('status');
+            $table->text('staff_message');
+            $table->dateTime('decided_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
