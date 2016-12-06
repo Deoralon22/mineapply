9ffd4f2ecda4db1a7164570474e27b2ebca6e19b
Apecengo <apecengo@gmail.com>
2016-12-06 17:02:07 +0100

    Add application summary to status check

--- resources/views/application/status.blade.php
+++ resources/views/application/status.blade.php
@@ -3,6 +3,10 @@
 @section('content')
 <div class="section">
 	<div class="container">
+
+
+
+
 	@if($application->getStatus() == 0)
 		<div class="card-panel">
 			<small><span class="right">{{ $application->getLastUpdatedDiff() }}</span></small>
@@ -32,6 +36,28 @@
 			<p>{{ $application->staff_message }}</p>
 		</div>
 	@endif
+
+  <br>
+	<h5>Your application</h5>
+	<br>
+      <div class="row">
+        <div class="input-field col s12">
+          <input readonly id="username" type="text" value="{{ $application->username }}" required>
+          <label for="username">Minecraft Username</label>
+        </div>
+      </div>
+	      <div class="row">
+	        <div class="input-field col s12">
+	          <textarea id="message" class="materialize-textarea" readonly>{{$application->message}}</textarea>
+	          <label for="message">Application message</label>
+	        </div>
+	      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" readonly value="{{ $application->email }}">
+          <label for="email" >E-mail</label>
+        </div>
+      </div>
 
 	</div>
 </div>
